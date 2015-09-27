070ebdefede95c96614dcb9950265dff60637bab
equeim <equeim@gmail.com>
2015-09-27 20:59:25 +0300

    Update FolderListModel from Qt 5.5.0

--- src/folderlistmodel/qquickfolderlistmodel.cpp
+++ src/folderlistmodel/qquickfolderlistmodel.cpp
@@ -1,39 +1,31 @@
 /****************************************************************************
 **
-** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
-** Contact: http://www.qt-project.org/legal
+** Copyright (C) 2015 The Qt Company Ltd.
+** Contact: http://www.qt.io/licensing/
 **
 ** This file is part of the examples of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:LGPL$
+** $QT_BEGIN_LICENSE:LGPL21$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Digia.  For licensing terms and
-** conditions see http://qt.digia.com/licensing.  For further information
-** use the contact form at http://qt.digia.com/contact-us.
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
 **
 ** GNU Lesser General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 as published by the Free Software
-** Foundation and appearing in the file LICENSE.LGPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU Lesser General Public License version 2.1 requirements
-** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** General Public License version 2.1 or version 3 as published by the Free
+** Software Foundation and appearing in the file LICENSE.LGPLv21 and
+** LICENSE.LGPLv3 included in the packaging of this file. Please review the
+** following information to ensure the GNU Lesser General Public License
+** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
-** In addition, as a special exception, Digia gives you certain additional
-** rights.  These rights are described in the Digia Qt LGPL Exception
+** As a special exception, The Qt Company gives you certain additional
+** rights. These rights are described in The Qt Company LGPL Exception
 ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3.0 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.  Please review the following information to
-** ensure the GNU General Public License version 3.0 requirements will be
-** met: http://www.gnu.org/copyleft/gpl.html.
-**
 **
 ** $QT_END_LICENSE$
 **
@@ -96,10 +88,10 @@ void QQuickFolderListModelPrivate::init()
 {
     Q_Q(QQuickFolderListModel);
     qRegisterMetaType<QList<FileProperty> >("QList<FileProperty>");
-    q->connect(&fileInfoThread, SIGNAL(directoryChanged(QString, QList<FileProperty>)),
-               q, SLOT(_q_directoryChanged(QString, QList<FileProperty>)));
-    q->connect(&fileInfoThread, SIGNAL(directoryUpdated(QString, QList<FileProperty>, int, int)),
-               q, SLOT(_q_directoryUpdated(QString, QList<FileProperty>, int, int)));
+    q->connect(&fileInfoThread, SIGNAL(directoryChanged(QString,QList<FileProperty>)),
+               q, SLOT(_q_directoryChanged(QString,QList<FileProperty>)));
+    q->connect(&fileInfoThread, SIGNAL(directoryUpdated(QString,QList<FileProperty>,int,int)),
+               q, SLOT(_q_directoryUpdated(QString,QList<FileProperty>,int,int)));
     q->connect(&fileInfoThread, SIGNAL(sortFinished(QList<FileProperty>)),
                q, SLOT(_q_sortFinished(QList<FileProperty>)));
     q->connect(q, SIGNAL(rowCountChanged()), q, SIGNAL(countChanged()));
@@ -158,35 +150,28 @@ void QQuickFolderListModelPrivate::_q_directoryUpdated(const QString &directory,
     Q_UNUSED(directory);
 
     QModelIndex parent;
-    if (data.size() > list.size()) {
-        //File(s) removed. Since I do not know how many
-        //or where I need to update the whole list from the first item.
+    if (data.size() == list.size()) {
+        QModelIndex modelIndexFrom = q->createIndex(fromIndex, 0);
+        QModelIndex modelIndexTo = q->createIndex(toIndex, 0);
         data = list;
-        q->beginRemoveRows(parent, fromIndex, toIndex);
-        q->endRemoveRows();
+        emit q->dataChanged(modelIndexFrom, modelIndexTo);
+    } else {
+        // File(s) inserted or removed. Since I do not know how many
+        // or where, I need to update the whole list from the first item.
+        // This is a little pessimistic, but optimizing it would require
+        // more information in the signal from FileInfoThread.
+        if (data.size() > 0) {
+            q->beginRemoveRows(parent, 0, data.size() - 1);
+            q->endRemoveRows();
+        }
+        data = list;
         if (list.size() > 0) {
-            q->beginInsertRows(parent, fromIndex, list.size()-1);
+            if (toIndex > list.size() - 1)
+                toIndex = list.size() - 1;
+            q->beginInsertRows(parent, 0, data.size() - 1);
             q->endInsertRows();
         }
         emit q->rowCountChanged();
-    } else if (data.size() < list.size()) {
-        //qDebug() << "File added. FromIndex: " << fromIndex << " toIndex: " << toIndex << " list size: " << list.size();
-        //File(s) added. Calculate how many and insert
-        //from the first changed one.
-        toIndex = fromIndex + (list.size() - data.size()-1);
-        q->beginInsertRows(parent, fromIndex, toIndex);
-        q->endInsertRows();
-        data = list;
-        emit q->rowCountChanged();
-        QModelIndex modelIndexFrom = q->createIndex(fromIndex, 0);
-        QModelIndex modelIndexTo = q->createIndex(toIndex, 0);
-        emit q->dataChanged(modelIndexFrom, modelIndexTo);
-    } else {
-        //qDebug() << "File has been updated";
-        QModelIndex modelIndexFrom = q->createIndex(fromIndex, 0);
-        QModelIndex modelIndexTo = q->createIndex(toIndex, 0);
-        data = list;
-        emit q->dataChanged(modelIndexFrom, modelIndexTo);
     }
 }
 
@@ -453,7 +438,7 @@ void QQuickFolderListModel::setFolder(const QUrl &folder)
 
 
 /*!
-   \qmlproperty url QQuickFolderListModel::rootFolder
+   \qmlproperty url FolderListModel::rootFolder
 
    When the rootFolder is set, then this folder will
    be threated as the root in the file system, so that
@@ -486,7 +471,7 @@ void QQuickFolderListModel::setRootFolder(const QUrl &path)
 /*!
     \qmlproperty url FolderListModel::parentFolder
 
-    Returns the URL of the parent of of the current \l folder.
+    Returns the URL of the parent of the current \l folder.
 */
 QUrl QQuickFolderListModel::parentFolder() const
 {
@@ -543,8 +528,8 @@ void QQuickFolderListModel::classBegin()
 void QQuickFolderListModel::componentComplete()
 {
     Q_D(QQuickFolderListModel);
-
-    if (!d->currentDir.isValid() || !d->currentDir.isLocalFile() || !QDir().exists(d->currentDir.toLocalFile()))
+    QString localPath = QQmlFile::urlToLocalFileOrQrc(d->currentDir);
+    if (localPath.isEmpty() || !QDir(localPath).exists())
         setFolder(QUrl::fromLocalFile(QDir::currentPath()));
 }
 
@@ -556,7 +541,7 @@ void QQuickFolderListModel::componentComplete()
     \list
     \li Unsorted - no sorting is applied.
     \li Name - sort by filename
-    \li LastModified - sort by time modified
+    \li Time - sort by time modified
     \li Size - sort by file size
     \li Type - sort by file type (extension)
     \endlist
